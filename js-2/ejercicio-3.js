2f7713e6f7b3bb2f29d0cdc5c90d728af8b4d050
walter <wrangelfuentes@gmail.com>
2023-03-14 21:42:43 -0500

    Solucion 3 a ejercicios de js

--- js-2/ejercicio-3.js
+++ js-2/ejercicio-3.js
@@ -1,4 +1,17 @@
-
+function maxIndex(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+    let maxIndex = 0;
+    let nunMayor = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > nunMayor) {
+            maxIndex = i;
+            nunMayor = arr[i];
+        }
+    }
+    return maxIndex;
+}
 
 console.log(maxIndex([1, 3, 2])) // 1
 console.log(maxIndex([10, 9, 8, 7, 6, 5, 4])) // 0
